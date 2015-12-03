f4a99d278b2db08fab5512fc2a809faa0555b379
Adrian Li <li.adrianmc@gmail.com>
2015-12-04 07:05:20 +0800

    new stuff

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready( function() {
 
-  // Initializing tooltips
+  // Initializing bootstrap dynamic widgets
+  $('#myCarousel').carousel();
   $('[data-toggle="popover"]').popover(); 
   $('[data-toggle="tooltip"]').tooltip(); 
   
@@ -18,49 +19,5 @@ $(document).ready( function() {
     $('html, body').animate({
       scrollTop: $('#faq').offset().top
     }, 1000);
-  });
-
-  // Fade navbar opaque
-  $(document).scroll(function(){
-    scrollPos = $(window).scrollTop();
-    bar = $('.navbar')
-    startFadePos = 20
-    endFadePos = 200
-    if (scrollPos > startFadePos) {
-      factor = (scrollPos - startFadePos) / (endFadePos - startFadePos)
-      bar.css('background-color', 'rgba(60,60,60,' + factor + ')')
-    } else if (scrollPos > startFadePos && scrollPos < endFadePos) {
-      bar.css('background-color', 'rgba(60,60,60,1)')
-    } else {
-      bar.css('background-color', 'transparent')
-    }
-  });
-
-  // preview cards section
-  $('#card-container').cubeportfolio({
-    gapHorizontal: 20,
-    gapVertical: 20,
-    mediaQueries:[{
-      width: 1000,
-      cols: 4
-    },{
-      width: 480,
-      cols: 2
-    },{
-      width: 360,
-      cols: 1
-    }]
-  });
-
-  // FAQ section
-  $('#js-grid-faq').cubeportfolio({
-    filters: '#js-filters-faq',
-    defaultFilter: '*',
-    animationType: 'sequentially',
-    gridAdjustment: 'default',
-    displayType: 'default',
-    caption: 'expand',
-    gapHorizontal: 0,
-    gapVertical: 0
   });
 });
