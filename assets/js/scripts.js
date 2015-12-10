4a89ee8cde09e052eaa1417687eed5c0b9f286fb
Adrian Li <li.adrianmc@gmail.com>
2015-12-11 04:00:52 +0800

    scripts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,4 +20,32 @@ $(document).ready( function() {
       scrollTop: $('#faq').offset().top
     }, 1000);
   });
+
+  // preview cards section
+  $('#card-container').cubeportfolio({
+    gapHorizontal: 20,
+    gapVertical: 20,
+    mediaQueries:[{
+      width: 1000,
+      cols: 4
+    },{
+      width: 480,
+      cols: 2
+    },{
+      width: 360,
+      cols: 1
+    }]
+  });
+
+  // FAQ section
+  $('#js-grid-faq').cubeportfolio({
+    filters: '#js-filters-faq',
+    defaultFilter: '*',
+    animationType: 'sequentially',
+    gridAdjustment: 'default',
+    displayType: 'default',
+    caption: 'expand',
+    gapHorizontal: 0,
+    gapVertical: 0
+  });
 });
